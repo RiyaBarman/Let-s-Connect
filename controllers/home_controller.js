b38e3cf50d7e62f3011717c8f33e257aa3305086
riya barman <riyab.ic@gmail.com>
2021-07-25 22:43:36 +0530

    showing comments below every post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,8 +4,14 @@ module.exports.home=function(req,res){
 
    // return res.end("<h1>module is working with controller</h1>");
    //populate the user of each post to get every attribute of user
-   Post.find({}).populate('user')
-   .populate('post')
+   Post.find({})
+   .populate('user')
+   .populate({
+      path:'comments',
+         populate: {
+            path:'user'
+         }
+   })
    .exec(function(err,posts){
 
     return res.render('home',{
