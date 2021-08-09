451de6b29a27db7e1efd54952d2e52c856e660cb
Riya Barman <87125700+RiyaBarman@users.noreply.github.com>
2021-08-10 02:04:43 +0530

    Update entrypoint.js

--- entrypoint.js
+++ entrypoint.js
@@ -1,42 +1,4 @@
-// const express=require('express');
-// //const cookieParser = require('cookie-parser');
-// const app=express();
-// const port=8000;
 
-// const bodyParser = require('body-parser');
-// const db = require('./configure/mongoose');
-// app.use(express.json());
-// app.use(express.urlencoded({
-//   extended: true
-// }));
-
-// // const expressLayouts = require('express-ejs-layouts');
- 
-// // app.use(express.urlencoded());
-
-// // app.use(cookieParser());
-
-// // app.use(express.static('./assets'));
-
-// // app.use(expressLayouts);
-// // // extract style and scripts from sub pages into the layout
-// // app.set('layout extractStyles', true);
-// // app.set('layout extractScripts', true);
-
-// //USE EXPRESS ROUTER
-// app.use('/',require('./routers/index'));
-// //VIEW SET UP
-// app.set('view engine','ejs');
-// app.set('views','./views');
-
-
-// app.listen(port,function(err){
-//     if(err){
-//         console.log(`Error in running the server:${err}`);
-//         return;
-//     }
-//     console.log(`server is running on port:${port}`);
-// });
 const express = require('express');
 
 const cookieParser = require('cookie-parser');
